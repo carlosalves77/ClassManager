9dbe5eb7a64910bafd1d575a2bad9ce338b90a06
Carlos Alves <carlosaalves07@outlook.com>
2023-09-15 20:21:56 -0300

    feat: finish profile view.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.carlos.classmanager"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.carlos.classmanager"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -43,12 +43,10 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -59,14 +57,14 @@ dependencies {
     implementation("androidx.room:room-ktx:2.5.2")
 
     // lifecycle
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     implementation("com.squareup.picasso:picasso:2.71828")
 
     // Google Auth
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     //Glide
--- app/src/main/java/com/carlos/classmanager/ui/Profile.kt
+++ app/src/main/java/com/carlos/classmanager/ui/Profile.kt
@@ -3,14 +3,12 @@ package com.carlos.classmanager.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import com.bumptech.glide.Glide
 import com.carlos.classmanager.R
 import com.carlos.classmanager.databinding.ActivityProfileBinding
 import com.google.firebase.auth.FirebaseAuth
-import kotlin.math.log
 
 class Profile : AppCompatActivity(), View.OnClickListener {
 
@@ -46,6 +44,7 @@ class Profile : AppCompatActivity(), View.OnClickListener {
                 startActivity(Intent(this@Profile, Home::class.java))
                 finish()
                 overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+
             }
         })
     }
